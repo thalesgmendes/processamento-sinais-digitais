c0b5e67f619cf109012c1d7978603da4a148d9f0
thalesgmendes <thalesmendes551@gmail.com>
2025-11-11 03:30:46 -0300

    adicionando arquivos

--- codigo/passa_baixa.py
+++ codigo/passa_baixa.py
@@ -3,28 +3,51 @@ import matplotlib.pyplot as plt
 from scipy import signal
 import sympy as sp
 
-num = [10000]
-den = [1, 10000]
 
-system = signal.TransferFunction(num, den)
+def passa_baixa(num, den):
+    tf = signal.TransferFunction(num, den)
+    w, mag, phase = signal.bode(tf)
+    mag = 10**(mag/20)
+    s = sp.Symbol('s')
+    num_poly = sum(coef * s**i for i, coef in enumerate(num[::-1]))
+    den_poly = sum(coef * s**i for i, coef in enumerate(den[::-1]))
+    G = num_poly / den_poly
+    return w, mag, phase, G, tf 
 
-w, mag, phase = signal.bode(system)
 
-s = sp.Symbol('s')
-num_poly = sum(coef * s**i for i, coef in enumerate(num[::-1]))
-den_poly = sum(coef * s**i for i, coef in enumerate(den[::-1]))
-G = num_poly / den_poly
+f_in = 1
+tau = 2
+
+t = np.linspace(0, 20, 2000)
+
+x_t = np.cos(f_in * t)
+
+num = [1]
+den = [tau, 1]
+
+w, mag, phase, G, tf = passa_baixa(num, den)
+t, y, _ = signal.lsim(tf,x_t, t)
+
+
 print("G(s) =", G)
 
 plt.figure(figsize=(8, 6))
 
-plt.subplot(2, 1, 1)
+plt.subplot(3,1,1)
+plt.plot(t, x_t, label='Entrada (cosseno)')
+plt.plot(t, y, label='Saída do sistema')
+plt.xlabel('Tempo [s]')
+plt.ylabel('Amplitude')
+plt.legend()
+plt.grid()
+
+plt.subplot(3, 1, 2)
 plt.semilogx(w, mag)
 plt.title("Diagrama de Bode - SciPy")
-plt.ylabel("Magnitude (dB)")
+plt.ylabel("Amplitude")
 plt.grid(True, which="both", ls="--", lw=0.5)
 
-plt.subplot(2, 1, 2)
+plt.subplot(3, 1, 3)
 plt.semilogx(w, phase)
 plt.xlabel("Frequência (rad/s)")
 plt.ylabel("Fase (graus)")
@@ -32,3 +55,4 @@ plt.grid(True, which="both", ls="--", lw=0.5)
 
 plt.tight_layout()
 plt.show()
+
